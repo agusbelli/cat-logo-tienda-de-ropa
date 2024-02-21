3ea55b5480153566eb28a3c8e0c3ebed49221038
agusbelli <bellisonzia@gmail.com>
2024-02-20 22:51:40 -0300

    configuracion google sheets

--- catalogo/src/App.jsx
+++ catalogo/src/App.jsx
@@ -1,91 +1,34 @@
+import { useEffect, useState } from 'react';
+import { GoogleSpreadsheet } from 'google-spreadsheet';
+import Producto from './componentes/producto';
 import './CatalogoDigital.css';
 import logo from "../public/logo.png"
-import remera1 from "../public/remeras/remera1.jpeg"
-import remera2 from "../public/remeras/remera2.jpeg"
-import remera3 from "../public/remeras/remera3.jpeg"
-import remera4 from "../public/remeras/remera4.jpeg"
-import remera5 from "../public/remeras/remera5.jpeg"
-import remera6 from "../public/remeras/remera6.jpeg"
-import remera7 from "../public/remeras/remera7.jpeg"
-import remera8 from "../public/remeras/remera8.jpeg"
-import remera9 from "../public/remeras/remera9.jpeg"
 
+import credentials from './credentials/client_secret_651032404759-rjjjqnfhfsmufvptaembju559673rgc2.apps.googleusercontent.com.json'; // Ruta al archivo JSON de credenciales
 
-
-
-
-
-
-import Producto from './componentes/producto'
+const SPREADSHEET_ID = '1xbD2v9B6xq52aZyS-vO5w8L0K23fzDX1qevKu5hRRe8';
 
 const App = () => {
-  // Arreglo de objetos de ejemplo para probar los componentes
-  const productos = [
-    {
-      id: 1,
-      nombre: 'Remera blanca',
-      foto: remera1,
-      fotoDorso: remera1,
-      disponible: true
-    },
-    {
-      id: 2,
-      nombre: 'Remera negra',
-      foto: remera2,
-      fotoDorso: remera2,
-      disponible: true
-    },
-    {
-      id: 3,
-      nombre: 'Remera azul',
-      foto: remera3,
-      fotoDorso: remera3,
-      disponible: false
-    },
-    {
-      id: 4,
-      nombre: 'Remera blanca',
-      foto: remera4,
-      fotoDorso: remera4,
-      disponible: true
-    },
-    {
-      id: 5,
-      nombre: 'Remera negra',
-      foto: remera5,
-      fotoDorso: remera5,
-      disponible: true
-    },
-    {
-      id: 6,
-      nombre: 'Remera azul',
-      foto: remera6,
-      fotoDorso: remera6,
-      disponible: false
-    },
-    {
-      id: 7,
-      nombre: 'Remera blanca',
-      foto: remera7,
-      fotoDorso: remera7,
-      disponible: true
-    },
-    {
-      id: 8,
-      nombre: 'Remera negra',
-      foto: remera8,
-      fotoDorso: remera8,
-      disponible: true
-    },
-    {
-      id: 9,
-      nombre: 'Remera azul',
-      foto: remera9,
-      fotoDorso: remera9,
-      disponible: false
+  const [productos, setProductos] = useState([]);
+
+  useEffect(() => {
+    async function getProducts() {
+      const doc = new GoogleSpreadsheet(SPREADSHEET_ID);
+      await doc.useServiceAccountAuth(credentials);
+      await doc.loadInfo();
+      const sheet = doc.sheetsByIndex[0];
+      const rows = await sheet.getRows();
+      const data = rows.map(row => ({
+        id: row.id,
+        nombre: row.nombre,
+        foto: row.foto,
+        fotoDorso: row.fotoDorso,
+        disponible: row.disponible === 'true',
+      }));
+      setProductos(data);
     }
-    // Agrega más objetos de producto según sea necesario
-  ];
+    getProducts();
+  }, []);
 
   return (
     <div className="catalogo-container">
