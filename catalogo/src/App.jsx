00a2407329873a24cf63bf2012d264e85501be61
agusbelli <bellisonzia@gmail.com>
2024-02-20 21:37:26 -0300

    segundo commit

--- catalogo/src/App.jsx
+++ catalogo/src/App.jsx
@@ -1,35 +1,12 @@
-import { useEffect, useState } from 'react';
-import { GoogleSpreadsheet } from 'google-spreadsheet';
-import Producto from './componentes/producto';
 
-const SPREADSHEET_ID = 'TU_ID_DE_HOJA_DE_CALCULO';
-const CLIENT_EMAIL = 'TU_EMAIL_DE_SERVICIO';
-const PRIVATE_KEY = 'TU_CLAVE_PRIVADA';
+import Producto from './componentes/producto';
+import remera1 from "../public/remeras/remera1.jpeg"
+
 
 const App = () => {
-  const [productos, setProductos] = useState([]);
-
-  useEffect(() => {
-    async function getProducts() {
-      const doc = new GoogleSpreadsheet(SPREADSHEET_ID);
-      await doc.useServiceAccountAuth({
-        client_email: CLIENT_EMAIL,
-        private_key: PRIVATE_KEY,
-      });
-      await doc.loadInfo();
-      const sheet = doc.sheetsByIndex[0];
-      const rows = await sheet.getRows();
-      const data = rows.map(row => ({
-        id: row.id,
-        nombre: row.nombre,
-        foto: row.foto,
-        fotoDorso: row.fotoDorso,
-        disponible: row.disponible === 'true',
-      }));
-      setProductos(data);
-    }
-    getProducts();
-  }, []);
+  const productos = [
+    {id:1, nombre: "remera1", foto:remera1, fotoDorso: remera1 }
+  ]
 
   return (
     <div className="catalogo-container">
@@ -38,15 +15,6 @@ const App = () => {
         <h2 className='titulo'>NUESTRO CATALOGO</h2>
         {productos.map(producto => (
           <Producto key={producto.id} producto={producto} />
-        ))}
-      </div>
-      {/* Resto del código */}
-    </div>
-  );
-};
-
-export default App;
-
         ))}
       </div>
       <footer className="footer">
