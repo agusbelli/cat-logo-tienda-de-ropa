b06adb1073a449770ae77436382617caa4dd170e
agusbelli <bellisonzia@gmail.com>
2024-02-26 18:44:04 -0300

    cors

--- server/src/index.js
+++ server/src/index.js
@@ -4,9 +4,11 @@ import cors from "cors";
 import productRoutes from "./routes/products.routes.js";
 
 const app = express();
-
+const corsOptions = {
+    origin: 'https://carcass-urban-catalogo.vercel.app'
+  };
+app.use(cors(corsOptions));
 app.use(express.json());
-app.use(cors());
 
 app.use("/server", productRoutes);
 
