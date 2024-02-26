6d5597b164639e94079a9585e489631273ceb3cd
agusbelli <bellisonzia@gmail.com>
2024-02-26 17:15:17 -0300

    testing rutas host

--- server/src/index.js
+++ server/src/index.js
@@ -8,7 +8,13 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-app.use("/server", productRoutes);
+/* app.use("/server", productRoutes);
+ */
+
+app.use("/server", (req, res)=>{
+    res.send("hola")
+});
+
 
 app.listen(3000);
 console.log("Server on port", 3000);
