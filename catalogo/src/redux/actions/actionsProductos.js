091f7f9f8ad3ec311ce4438987aad5f341b5f036
agusbelli <bellisonzia@gmail.com>
2024-02-26 18:37:42 -0300

    agregar url  del server

--- catalogo/src/redux/actions/actionsProductos.js
+++ catalogo/src/redux/actions/actionsProductos.js
@@ -2,11 +2,12 @@ import axios from "axios";
 
 export const GET_PRODUCTOS = "GET_PRODUCTOS"
 export const GET_PRODUCTO = "GET_PRODUCTO"
+const urlServer = "https://catalogo-carcassurban-server.vercel.app/server/"
 
 
 export const getProductos = () => {
     return async function (dispatch){
-        const data = await axios.get(`http://localhost:3000/server/products`);
+        const data = await axios.get(`${urlServer}/products`);
         const productos = data.data;
         dispatch({ type: GET_PRODUCTOS, payload: productos });
     };
@@ -15,11 +16,11 @@ export const getProductos = () => {
 export const postProducto = (newData) => {
     return async function (dispatch){
         try {
-            const data = await axios.post(`http://localhost:3000/server/products`, newData);
+            const data = await axios.post(`${urlServer}/products`, newData);
             console.log(data);
             const redireccionar = ()=>{return window.location='/carcass-nuevoproducto'}; 
             //si sale bien pido todo de nuevo
-            const data2 = await axios.get(`http://localhost:3000/server/products`);
+            const data2 = await axios.get(`${urlServer}/products`);
             const productos = data2.data;            
             dispatch({ type: GET_PRODUCTOS, payload: productos });
             alert("¡Producto  creado correctamente!");
@@ -35,11 +36,11 @@ export const postProducto = (newData) => {
 export const putProducto = (newData, id) => {
     return async function (dispatch){
         try {
-            const data = await axios.put(`http://localhost:3000/server/products/${id}`, newData);
+            const data = await axios.put(`${urlServer}/products/${id}`, newData);
             console.log(data);
             const redireccionar = ()=>{return window.location='/'}; 
             //si sale bien pido todo de nuevo
-            const data2 = await axios.get(`http://localhost:3000/server/products`);
+            const data2 = await axios.get(`${urlServer}/products`);
             const productos = data2.data;            
             dispatch({ type: GET_PRODUCTOS, payload: productos });
             redireccionar()
@@ -53,11 +54,11 @@ export const putProducto = (newData, id) => {
 export const eliminarProducto = (token, id) => {
     const tokenFront = token;
     return async function (dispatch){
-        const data = await axios.delete(`http://localhost:3000/server/products/${id}?tokenFront=${tokenFront}`);
+        const data = await axios.delete(`${urlServer}/products/${id}?tokenFront=${tokenFront}`);
         console.log(data);
         const redireccionar = ()=>{return window.location='/carcass-edicion'}; 
         //si sale bien pido todo de nuevo
-        const data2 = await axios.get(`http://localhost:3000/server/products`);
+        const data2 = await axios.get(`${urlServer}/products`);
         const productos = data2.data;            
         dispatch({ type: GET_PRODUCTOS, payload: productos });
         alert("¡Producto  eliminado correctamente!");
@@ -69,7 +70,7 @@ export const eliminarProducto = (token, id) => {
 
 export const getProductoId = (id) => {
     return async function (dispatch){
-        const data = await axios.get(`http://localhost:3000/server/products/${id}`);
+        const data = await axios.get(`${urlServer}/products/${id}`);
         const producto = data.data;
         dispatch({ type: GET_PRODUCTO, payload: producto });
     };
--- catalogo/src/redux/actions/actionsToкen.js
+++ catalogo/src/redux/actions/actionsToкen.js
@@ -1,11 +1,11 @@
 import axios from "axios";
 
 export const GET_TOKEN = "GET_TOKEN"
-
+const urlServer = "https://catalogo-carcassurban-server.vercel.app/server/"
 export const getToken = (userData) => {
     return async function (dispatch){
         const datos = userData;
-        const data = await axios.post(`http://localhost:3000/server/login`, datos );
+        const data = await axios.post(`${urlServer}/login`, datos );
         const codigo = data.data;
         dispatch({ type: GET_TOKEN, payload: codigo });
     };
