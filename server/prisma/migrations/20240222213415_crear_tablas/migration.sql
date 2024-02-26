26b185bee79a48725dfaf1e99329a79e1c02c1bb
agusbelli <bellisonzia@gmail.com>
2024-02-26 16:30:24 -0300

    nueva db

--- server/prisma/migrations/20240222213415_crear_tablas/migration.sql
+++
@@ -1,8 +0,0 @@
--- CreateTable
-CREATE TABLE "Product" (
-    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-    "nombre" TEXT NOT NULL,
-    "estado" BOOLEAN NOT NULL DEFAULT true,
-    "foto" TEXT NOT NULL,
-    "fotoDorso" TEXT NOT NULL
-);
